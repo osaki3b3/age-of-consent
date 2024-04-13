07760fe67335641d374009eef903350c02b0e18b
AREN G <42050331+adrianbengoechea@users.noreply.github.com>
2024-04-12 20:10:16 -0600

    quick test

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -16,71 +16,71 @@ import db from "../db.server";
 
 export async function loader ( { request } ) {
 
-  await authenticate.admin(request);
+  // await authenticate.admin(request);
 
-  let settings = await db.settings.findUnique({
-    where:{
-      id: 'EnabledCheckout18yoModal'
-    }
-  });
+  // let settings = await db.settings.findUnique({
+  //   where:{
+  //     id: 'EnabledCheckout18yoModal'
+  //   }
+  // });
 
-  return json( 
-    { 
-      apiKey: process.env.SHOPIFY_API_KEY || "", 
-      checked: ( settings.value == 'active' ? true : false ) 
-    } 
-  );
+  // return json( 
+  //   { 
+  //     apiKey: process.env.SHOPIFY_API_KEY || "", 
+  //     checked: ( settings.value == 'active' ? true : false ) 
+  //   } 
+  // );
 
 };
 
 export async function action( { request } ){
 
-  let settings = await request.formData();
+  // let settings = await request.formData();
 
-  settings = Object.fromEntries( settings.entries() );
+  // settings = Object.fromEntries( settings.entries() );
 
-  await db.settings.upsert({
-    where: {
-      id: "EnabledCheckout18yoModal",
-    },
-    update: {
-      id: 'EnabledCheckout18yoModal',
-      value: ( settings.modalCheckbox ) ? settings.modalCheckbox : 'false'
-    },
-    create: {
-      id: 'EnabledCheckout18yoModal',
-      value: ( settings.modalCheckbox ) ? settings.modalCheckbox : 'false'
-    }
-  });
+  // await db.settings.upsert({
+  //   where: {
+  //     id: "EnabledCheckout18yoModal",
+  //   },
+  //   update: {
+  //     id: 'EnabledCheckout18yoModal',
+  //     value: ( settings.modalCheckbox ) ? settings.modalCheckbox : 'false'
+  //   },
+  //   create: {
+  //     id: 'EnabledCheckout18yoModal',
+  //     value: ( settings.modalCheckbox ) ? settings.modalCheckbox : 'false'
+  //   }
+  // });
 
-  return json( settings );
+  // return json( settings );
 
 }
 
 export default function AppSettings(){
 
-  const settings = useLoaderData();
-  const [formState, setFormState] = useState(settings);
+  // const settings = useLoaderData();
+  // const [formState, setFormState] = useState(settings);
 
   return (
 
-    <AppProvider isEmbeddedApp apiKey={ formState.apiKey }>
-      <Form method="POST">
-            <Page title="General" primaryAction={<Button submit={true}>Save</Button>}>
-              <Card>
+    // <AppProvider isEmbeddedApp apiKey={ formState.apiKey }>
+    //   <Form method="POST">
+    //         <Page title="General" primaryAction={<Button submit={true}>Save</Button>}>
+    //           <Card>
 
                 <Checkbox
                   label="Enable NYC AgeCheck Popup"
                   name="modalCheckbox"
                   value="active"
-                  checked={formState.checked}
-                  onChange={( checked ) => setFormState({ ...formState, checked: checked })}
+                  // checked={formState.checked}
+                  // onChange={( checked ) => setFormState({ ...formState, checked: checked })}
                 />
 
-              </Card>
-            </Page>
-      </Form>
-    </AppProvider>
+    //           </Card>
+    //         </Page>
+    //   </Form>
+    // </AppProvider>
 
   );
 
