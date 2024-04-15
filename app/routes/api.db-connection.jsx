b0d8779c128124f4ac2cfe9b2893f9e3aab9a703
Adrian Bengoechea <ebel.bengoechea@gmail.com>
2024-04-15 12:42:18 -0600

    second prisma test

--- app/routes/api.db-connection.jsx
+++ app/routes/api.db-connection.jsx
@@ -11,16 +11,25 @@ export const loader = async ({ request }) => {
 
     const prisma = new PrismaClient();
 
+    let count = await prisma.settings.count();
+    // let settings = await prisma.settings.findUnique({
+    //     where:{
+    //         id: 'EnabledCheckout18yoModal'
+    //     }
+    // });
+
     // await authenticate.admin(request);
 
-    return null;
-
+    return count;
+ 
 };
 
   
 export default function Index() {
 
     const settings = useLoaderData();
+
+    console.log( '> settings: ', settings )
 
     return ( 
         <Page>
