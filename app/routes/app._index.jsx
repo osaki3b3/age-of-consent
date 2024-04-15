7ec9d9123074af1d15aed162c3270dbc60fc5bfe
AREN G <42050331+adrianbengoechea@users.noreply.github.com>
2024-04-14 18:16:58 -0600

    disable new prismaClient()

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -14,7 +14,7 @@ export const loader = async ({ request }) => {
 
   await authenticate.admin(request);
 
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
 
   // const settingsEntries = await prisma.settings.count({});
 
